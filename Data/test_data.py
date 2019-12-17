1ec1c196df7bd093cee5325ded42d8a8f6acceb6
svdior <svdior@gmail.com>
2019-12-17 15:42:49 +0200

    Initial commit

---
+++ Data/test_data.py
@@ -0,0 +1,21 @@
+import random
+import os
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+class CreateEventData():
+
+
+    TITLE = random.choice( ['New Year', 'Christmas', 'Malanka'] )
+    IMAGE = os.path.join( current_path,  'imageAddEvent\\party.jpg' )
+    LOGIN_USER = 'user@gmail.com'
+    PASSWORD_USER = '1qaz1qaz'
+    DESCRIPTION = {"New Year": "Happy 2020 Year!!!Weclome to the Party"}
+    ATT_DATA = "innerHtml"
+
+
+
+
+
+
